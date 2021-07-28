52e3495e5752bc922b319be1a098e8d9faa8948a
wilsonmoura <wilsonsmoura@gmail.com>
2021-07-28 16:06:38 -0300

    Fixed #25 -- Technical Matters controlled list

--- symphony/src/Proethos2/CoreBundle/Controller/CRUDController.php
+++ symphony/src/Proethos2/CoreBundle/Controller/CRUDController.php
@@ -38,6 +38,7 @@ use Proethos2\ModelBundle\Entity\BestPracticeRole;
 use Proethos2\ModelBundle\Entity\BestPracticeEntity;
 use Proethos2\ModelBundle\Entity\Institution;
 use Proethos2\ModelBundle\Entity\Stakeholder;
+use Proethos2\ModelBundle\Entity\TechnicalMatter;
 
 
 class CRUDController extends Controller
@@ -3576,6 +3577,125 @@ class CRUDController extends Controller
 
             $session->getFlashBag()->add('success', $translator->trans("Item updated with success."));
             return $this->redirectToRoute('crud_admin_controlled_list_stakeholder_list', array(), 301);
+        }
+
+        return $output;
+    }
+
+    /**
+     * @Route("/admin/controlled-list/technical-matter", name="crud_admin_controlled_list_technical_matter_list")
+     * @Template()
+     */
+    public function listControlledListTechnicalMatterAction()
+    {
+        $output = array();
+        $request = $this->getRequest();
+        $session = $request->getSession();
+        $translator = $this->get('translator');
+        $em = $this->getDoctrine()->getManager();
+
+        $item_repository = $em->getRepository('Proethos2ModelBundle:TechnicalMatter');
+        $trans_repository = $em->getRepository('Gedmo\\Translatable\\Entity\\Translation');
+
+        $items = $item_repository->findAll();
+        $output['items'] = $items;
+
+        // checking if was a post request
+        if($this->getRequest()->isMethod('POST')) {
+
+            // getting post data
+            $post_data = $request->request->all();
+
+            // checking required files
+            foreach(array('name') as $field) {
+
+                if(!isset($post_data[$field]) or empty($post_data[$field])) {
+                    $session->getFlashBag()->add('error', $translator->trans("Field '%field%' is required.", array("%field%" => $field)));
+                    return $output;
+                }
+            }
+
+            $item = new TechnicalMatter();
+            $item->setTranslatableLocale('en');
+
+            $item->setName($post_data['name']);
+
+            foreach(array('pt_BR', 'es_ES', 'fr_FR') as $locale) {
+                if(!empty($post_data["name-$locale"])) {
+                    $trans_repository = $trans_repository->translate($item, 'name', $locale, $post_data["name-$locale"]);
+                }
+            }
+
+            $em->persist($item);
+            $em->flush();
+
+            $session->getFlashBag()->add('success', $translator->trans("Item created with success."));
+            return $this->redirectToRoute('crud_admin_controlled_list_technical_matter_list', array(), 301);
+        }
+
+        return $output;
+    }
+
+    /**
+     * @Route("/admin/controlled-list/technical-matter/{item_id}", name="crud_admin_controlled_list_technical_matter_update")
+     * @Template()
+     */
+    public function updateControlledListTechnicalMatterAction($item_id)
+    {
+        $output = array();
+        $request = $this->getRequest();
+        $session = $request->getSession();
+        $translator = $this->get('translator');
+        $em = $this->getDoctrine()->getManager();
+
+        $item_repository = $em->getRepository('Proethos2ModelBundle:TechnicalMatter');
+        $trans_repository = $em->getRepository('Gedmo\\Translatable\\Entity\\Translation');
+
+        $item = $item_repository->find($item_id);
+
+        if (!$item) {
+            throw $this->createNotFoundException($translator->trans('No item found'));
+        }
+        $output['item'] = $item;
+
+        $translations = $trans_repository->findTranslations($item);
+        $output['translations'] = $translations;
+
+        // checking if was a post request
+        if($this->getRequest()->isMethod('POST')) {
+
+            // getting post data
+            $post_data = $request->request->all();
+
+            // checking required files
+            foreach(array('name') as $field) {
+                if(!isset($post_data[$field]) or empty($post_data[$field])) {
+                    $session->getFlashBag()->add('error', $translator->trans("Field '%field%' is required.", array("%field%" => $field)));
+                    return $output;
+                }
+            }
+
+            $item->setTranslatableLocale('en');
+
+            $item->setName($post_data['name']);
+
+            foreach(array('pt_BR', 'es_ES', 'fr_FR') as $locale) {
+                if(!empty($post_data["name-$locale"])) {
+                    $trans_repository = $trans_repository->translate($item, 'name', $locale, $post_data["name-$locale"]);
+                }
+            }
+
+            if(isset($post_data['status']) and $post_data['status'] == "true") {
+                $item->setStatus(true);
+            } else {
+                $item->setStatus(false);   
+            }
+
+            $em->persist($item);
+            $em->flush();
+
+            $session->getFlashBag()->add('success', $translator->trans("Item updated with success."));
+            return $this->redirectToRoute('crud_admin_controlled_list_technical_matter_list', array(), 301);
         }
 
         return $output;
