55ed9cc48a78222d09eedbab095c340be264f2a4
wilsonmoura <wilsonsmoura@gmail.com>
2021-11-22 23:17:33 -0300

    Update fixtures

---
+++ symphony/fixtures/data_list_intervention.sql
@@ -0,0 +1,4 @@
+REPLACE INTO `list_intervention` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (1,'2021-07-02 00:12:31','2021-07-02 00:12:31','Prevention – Risk reduction','prevention-risk-reduction',1);
+REPLACE INTO `list_intervention` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (2,'2021-07-02 00:12:55','2021-07-02 00:12:55','Emergencies and Preparedness – International Health Regulations','emergencies-and-preparedness-international-health-regulations',1);
+REPLACE INTO `list_intervention` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (3,'2021-07-02 00:13:46','2021-07-02 00:13:46','Innovation','innovation',1);
+REPLACE INTO `list_intervention` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (4,'2021-07-02 00:13:59','2021-07-02 00:13:59','Problem solving','problem-solving',1);
---
+++ symphony/fixtures/data_list_population_group.sql
@@ -0,0 +1,10 @@
+REPLACE INTO `list_population_group` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (1,'2021-07-02 00:19:15','2021-07-02 00:19:15','Indigenous population','indigenous-population',1);
+REPLACE INTO `list_population_group` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (2,'2021-07-02 00:19:22','2021-07-02 00:19:22','African American population','african-american-population',1);
+REPLACE INTO `list_population_group` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (3,'2021-07-02 00:19:38','2021-07-02 00:19:38','Elderly population','elderly-population',1);
+REPLACE INTO `list_population_group` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (4,'2021-07-02 00:19:48','2021-07-02 00:19:48','Vulnerable population','vulnerable-population',1);
+REPLACE INTO `list_population_group` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (5,'2021-07-27 16:40:45','2021-07-27 16:40:45','Not applicable','not-applicable',1);
+REPLACE INTO `list_population_group` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (6,'2021-07-27 16:58:36','2021-07-27 16:58:36','Women','women',1);
+REPLACE INTO `list_population_group` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (7,'2021-07-27 16:58:42','2021-07-27 16:58:42','Men','men',1);
+REPLACE INTO `list_population_group` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (8,'2021-07-27 16:58:55','2021-07-27 16:58:55','Adolescents/Youth','adolescents-youth',1);
+REPLACE INTO `list_population_group` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (9,'2021-07-27 16:59:27','2021-07-27 16:59:27','Children','children',1);
+REPLACE INTO `list_population_group` (`id`, `created`, `updated`, `name`, `slug`, `status`) VALUES (10,'2021-07-27 17:00:03','2021-07-27 17:00:03','Other','other',1);
