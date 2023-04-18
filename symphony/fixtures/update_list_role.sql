bf8ca4726d095944fd0a40dca14566dc3531b314
wilsonmoura <wilsonsmoura@gmail.com>
2023-04-18 13:45:52 -0300

    Update fixtures

--- symphony/fixtures/update_list_role.sql
+++
@@ -1,3 +0,0 @@
-UPDATE `list_role` SET `name` = 'Submitter' WHERE `id` = 1;
-UPDATE `list_role` SET `name` = 'Secretariat' WHERE `id` = 2;
-UPDATE `list_role` SET `name` = 'Reviewer' WHERE `id` = 3;
