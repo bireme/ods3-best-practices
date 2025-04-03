91f1fe6b565cb09b544cd8a7689e38f7f472f525
Do Carmo <docarmgab@paho.org>
2025-04-03 15:07:41 -0300

    Update SecurityController.php

--- symphony/src/Proethos2/CoreBundle/Controller/SecurityController.php
+++ symphony/src/Proethos2/CoreBundle/Controller/SecurityController.php
@@ -626,9 +626,8 @@ class SecurityController extends Controller
             $post_data = $request->request->all();
             $output['content'] = $post_data;
 
-
             // checking required fields
-            foreach(array('name', 'username', 'email', 'country', 'password', 'confirm-password', 'g-recaptcha-response') as $field) {   
+            foreach(array('name', 'username', 'email', 'country', 'password', 'confirm-password') as $field) {   
                 if(!isset($post_data[$field]) or empty($post_data[$field])) {
                     $session->getFlashBag()->add('error', $translator->trans("Field '%field%' is required.", array("%field%" => $field)));
                     return $output;
